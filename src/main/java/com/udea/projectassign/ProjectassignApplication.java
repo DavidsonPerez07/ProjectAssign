51e92da72f44cb2b2d3bb85bf0e61160323d695c
Davidson Pérez <trosquinalidad132@gmail.com>
2025-07-18 16:17:07 -0500

    project creation

---
+++ src/main/java/com/udea/projectassign/ProjectassignApplication.java
@@ -0,0 +1,13 @@
+package com.udea.projectassign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectassignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectassignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/projectassign/ProjectassignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.projectassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectassignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
